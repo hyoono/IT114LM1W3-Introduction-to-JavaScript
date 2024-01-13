b3af9005587c64f0e712ddf4898a0a83131d01e7
JOSHUA FIL .EVASCO <2022jfevasco@live.mcl.edu.ph>
2024-01-13 10:41:52 +0800

    Usable code that fulfills TODO's for JavaScript

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -4,44 +4,91 @@
 */
 
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
+console.log("-------------------------TODO 1.1 to 1.3---------------------------")
 // Your code here
+var x = "Hello World"
+console.log(x)
+
+console.log(y)
+var y = "Hello World"
+
+//Answer: The variable is undefined when it is declared after the console.log.
 
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
+let a = "Hello World"
+console.log(a)
+//Answer: let keyword is similar to var but is limited in the scope of its declaration.
 
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
+const b = 1
+//b = 2
+console.log (b)
+//Answer: Node.js throws an error as the const keyword is used for a constant declaration.
+
 
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer: var is used for declaring variables that is accessible globally and can be changed at runtime. let is limited to where it was declared.
+// const is used for declaring constant values and cannot be changed at runtime
 
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
+console.log("----------------------------TODO 1.4---------------------------")
+const c = 10
+const d = 20
+console.log("10 + 20 =", c + d)
+console.log("10 - 20 =", c - d)
+console.log("10 * 20 =", c * d)
+console.log("10 / 20 =", c / d)
+console.log("10 % 20 =", c % d)
+console.log("10 ** 20 =", c ** d)
 
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
+// Answer: I used the addition (+), subtraction (-), multiplication (*), division (/), modulus (%), and exponent (**) operators of JS
 // Your code here
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
+console.log("----------------------------TODO 1.5---------------------------")
 // Your code here
+var e =  "Good"
+let f = "Morning"
+f += " to you!"
+
+console.log(e + " " + f)
 
 // Checkpoint 1.3 What operators did you use?
-// Answer: 
+// Answer: I have used the concatenate operator (+) and the assignment operator (+=).
 
 // TODO 1.6 Try out the different logical operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
+console.log("----------------------------TODO 1.6---------------------------")
 // Your code here
-
+const g = 10 && 11
+const h = 10 || 11
+const i = !10
+console.log(g)
+console.log(h)
+console.log(i)
 // Checkpoint 1.4 What operators did you use?
-// Answer: 
+// Answer: I  have used the logical and (&&), logical or (||) and logical not(!) operators.
 
 
 // TODO 1.7 Try out the different relational operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
-
-
+console.log("----------------------------TODO 1.7---------------------------")
+console.log([] == false)
 // Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
-// Answer: 
+// Answer: It returns true. This is because the [] is an empty array and when compared to a boolean value, it is converted to a number. When it is converted to a number, it is converted to 0.
+//0 is a falsy value and hence, it returns true.
 // Your code here
+console.log("----------------------------TODO 1.7 (array)---------------------------")
+const j = ["Hello", "World", "alphabet", "random", "users"]
+const k = 0 in j
+const l = 6 in j
+const m = "alphabet" in j 
+console.log(k)
+console.log(l)
+console.log(m)
--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -7,50 +7,113 @@ const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock
 
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 // Your code here
+console.log("----------------------------TODO 2.1---------------------------");
+
+if (randomNumber > 50) {
+    console.log("more than fifty");
+}
 
 // TODO 2.2 Display whether the random number is odd or even
 // Your code here
+console.log("----------------------------TODO 2.2---------------------------");
+if (randomNumber % 2 == 0) {
+    console.log("even");
+}
+else {
+    console.log("odd");
+}
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
+console.log("----------------------------TODO 2.3---------------------------");
+if (randomNumber % 3 == 0 && randomNumber % 5 == 0) {
+    console.log("fizzbuzz");
+}
+else if (randomNumber % 3 == 0) {
+    console.log("fizz");
+}
+else if (randomNumber % 5 == 0) {
+    console.log("buzz");
+}
+else {
+    console.log(randomNumber);
+}
 
+console.log("----------------------------TODO 2.4---------------------------");
 let toDisplay = ""
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
+toDisplay = randomNumber % 2 == 0 ? "Even" : "Odd";
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: switch statements are usable if a variable is to be compared with multiple values. This is especially useful when validating input and creating menu for applications.
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
+console.log("----------------------------TODO 2.5---------------------------")
 const n = 10
 // Your code here
+for (let i = 1; i <= n; i++) {
+    console.log(i);
+}
 
+console.log("----------------------------TODO 2.6---------------------------")
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
 // Your code here
+let i = 0;
+while (i < list.length) {
+    console.log(list[i]);
+    i++;
+}
 
 // Checkpoint 2.3 How would you simulate a do-while loop in JavaScript
-// Answer: 
+// Answer: JavaScript has a do-while loop functionality part of its syntax. It follows the same format as other do-while loops in other languages.
 
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
+console.log("----------------------------TODO 2.7---------------------------")
+for (let i of list){
+    console.log(i);
+}
 
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
+console.log("----------------------------TODO 2.8---------------------------")
+for (let i in list){
+    console.log(list[i]);
+}
 
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
+console.log("----------------------------TODO 2.9---------------------------")
+list.forEach(element => console.log(element));
 
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
-// Answer: 
+// Answer: for-of loops are used when the index of the element is not needed. for-in loops are used when the index of the element is needed. They are useful if an array has to be modified on runtime.
+// forEach loops are used when the index of the element is not needed and the array is not to be modified. It is useful of listing out the array in sequence on the array
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
+console.log("----------------------------TODO 2.10---------------------------")
 const numerator = Math.floor((Math.random() * 100) + 1);
 const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
+try{
+    if (denominator == 0) {
+        throw "division by zero";
+    }
+    else{
+    const quotient = numerator / denominator;
+    console.log(quotient);}
+}
+catch (err) {
+    console.log("Error encountered")
+    console.log(err);
+}
+finally {
+    console.log("cleaning up resources");
+}
 
-const quotient = numerator / denominator;
 
